a5d3e1375240a8925022b22acd75e396f6f9a3a6
Donovan <donnie58744@gmail.com>
2026-02-19 16:11:59 -0700

    Example TTS
    Basic example on how to use the TTS function

--- examples/tts.py
+++ examples/tts.py
@@ -1,13 +1,12 @@
 import sys
 import os
 
-# Get the directory of THIS file
 current_dir = os.path.dirname(os.path.abspath(__file__))
+lib_path = os.path.normpath(os.path.join(current_dir, "../"))  # parent of pycozmo
+sys.path.insert(0, lib_path)  # insert at front to take priority
 
-# Go up one level to root, then into lib
-lib_path = os.path.join(current_dir, "../..", "pycozmo/")
-
-sys.path.append(lib_path)
+print("Looking in:", lib_path)
+print("pycozmo exists:", os.path.exists(os.path.join(lib_path, "pycozmo")))
 
 import pycozmo
 
@@ -17,6 +16,6 @@ if __name__ == "__main__":
     robot.start()
     robot.connect()
     robot.wait_for_robot()
-    robot.set_volume(3000)
+    robot.set_volume(5000)
     robot.say_text(txt="Hello World, My name is Cozmo!")
     robot.wait_for(pycozmo.event.EvtAudioCompleted)
